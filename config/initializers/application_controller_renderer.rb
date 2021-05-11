571ff479eccea86f82f6df3d9c74170a8a0e0312
sakasegawajunki <j.sakasegawa@gmail.com>
2021-05-11 05:05:30 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '777f731c519848639738678dc5626e92bba86bf8797fc0ca6ff0aefc6b79c0a2bf8c814cfbd0db4703d14749a3fcc8aa3fa1266bb9baaedd5d593436a3a14c18'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-root 'homes#top'
+  root 'homes#top'
+  devise_for :users
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 end
