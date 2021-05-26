026fad9cec7a43adb721e5dae1d507fb013b8fb5
sakasegawajunki <j.sakasegawa@gmail.com>
2021-05-26 01:04:36 +0000

    [Update]

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -5,11 +5,10 @@ class PostCommentsController < ApplicationController
     comment.post_image_id = post_image.id
     comment.save
     redirect_to post_image_path(post_image)
-
   end
 
   def destroy
-    PostComment.find_by(id:params[:id],post_image_id: params[:post_image_id]).destroy
+    PostComment.find_by(id: params[:id], post_image_id: params[:post_image_id]).destroy
     redirect_to post_image_path(params[:post_image_id])
   end
 
@@ -18,5 +17,4 @@ class PostCommentsController < ApplicationController
   def post_comment_params
     params.require(:post_comment).permit(:comment)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  
+  get 'tests/top'
   devise_for :users
   root 'homes#top'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :favorites,only: [:create,:destroy]
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-  resources :users,only: [:show,:edit,:update]
+  resources :users, only: [:show, :edit, :update]
 end
